c47d48ace0c58caac7dee1ea82498ed1bc60b8a3
Ram Srinivasan <doctorram@gmail.com>
2014-12-30 12:29:16 +0000

    I have added the Java file for the first time and put some info about the Nobel Laureate into README

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ This is the time for you to pack my box with five dozen liquor jugs.
 # some more text added here.
 Initializr is an HTML5 templates generator to help you getting started with a new project based on HTML5 Boilerplate. 
 It generates for you a clean customizable template with just what you need to start! 
+
+#Add even more stuff about the Nobel Laureate
+Kailash Satyarthi: The child right activist was conferred the 2014 Nobel Peace Prize along with Pakistan's Malala Yousafzai. 
+Nobel Laureate Satyarthi is the founder of 'Bachbapn Bachao Andolan' that works towards rescuing children from bonded labour. 
+So far he has saved more than 80, 000 children from child slavery in around 100 countries. His organisation 'Bachbapn Bachao Andolan' 
+was founded in 1980 and since then it has been instrumental in providing rehabilitation to rescued children. 
---
+++ test.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+public class JCFComDataClass {
+
+	public static String[] vehicles = {"Ford", "Chevrolet", "Volkswagen", "Volvo", "Mercedes"};
+	public static String[][] driversList = {{"Balu", "Govindan", "Mahendra"}, {"Ganepathy", "Solomon", "Thozhan", "Sakthivel"},
+		{"Kathir", "Simon", "Islamiyah"}, {"David", "Dhillu", "Babu"}, {"Raj", "Aasan", "Vasuki"}};
+
+	public static void main(String[] args) {
+		
+		Map<String, Set<String>> personnel = new HashMap<>();
+		
+		for(int i=0; i<vehicles.length; i++) {
+			String vehicle = vehicles[i];
+			String[] drivers = driversList[i];
+			Set<String> driverSet = new LinkedHashSet<String>();
+			
+			for(String driver: drivers) {
+				driverSet.add(driver);
+			}
+			System.out.println("Vehicle is "+vehicle+" and its drivers are "+driverSet+"\n");
+			
+			//Assigning a bunch of drivers to a vehicle
+			personnel.put(vehicle, driverSet);
+		}
+		
+		//Now we can query the map with a vehicle name and find the drivers
+		System.out.println("\nDrivers for Volkswagen are "+personnel.get("Volkswagen"));
+	}
+
+}
